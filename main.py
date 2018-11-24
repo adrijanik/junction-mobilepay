fa6d5cc84dff65f204a1d8bb9cac5bb9385f8d0a
EC2 Default User <ec2-user@ip-172-31-24-157.eu-west-1.compute.internal>
2018-11-24 22:00:58 +0000

    init

---
+++ main.py
@@ -0,0 +1,64 @@
+# -*- coding:utf8 -*-
+# !/usr/bin/env python
+
+import json
+
+from flask import Flask, request, make_response, jsonify
+
+
+app = Flask(__name__)
+log = app.logger
+
+
+@app.route('/', methods=['POST'])
+def webhook():
+    """This method handles the http requests for the Dialogflow webhook
+
+    """
+    print("RECEIVED")
+#    req = json.loads(request.json)#
+    req = get_json(silent=True, force=True)
+    print(req)
+    print(type(req))
+    res = "Nothig happened"
+    try:
+#        action = req['result']['action']#
+        action = req.get('result').get('action')
+    except AttributeError as e:
+        print("ERRRROR JSON")
+        print(e)
+        return 'json error'
+
+    if action == 'transfer.money.yes':
+        res = friend_transfer(req)
+
+    r = {'fulfillmentText': res}
+    print("WHAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAT?!")
+    print(res)
+    return make_response(jsonify({'speech': res})) #make_response(jsonify(r))# make_response(r)
+
+
+def handle_money(params):
+    response = "Hi there it worked!"
+    return response
+
+
+def friend_transfer(req):
+    """Returns a string containing text with a response to the user
+    """
+    parameters = req['result']['parameters']
+
+    response = "Not ready yet"
+    print('Dialogflow Parameters:')
+    print(json.dumps(parameters, indent=4))
+
+    try:
+        response = handle_money(parameters)
+    # return an error if there is an error getting the forecast
+    except (ValueError, IOError) as error:
+        return error
+#    response = "dummy"
+    return response
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
